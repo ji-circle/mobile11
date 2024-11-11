097002e90d17636d905deb75819277516e4576c6
ji-circle <sp202401jw@gmail.com>
2024-11-11 19:16:04 +0900

    first

---
+++ app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
@@ -0,0 +1,18 @@
+package com.example.fifthapp.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.fifthapp.Greeting
+
+@Composable
+fun LoginPage(){
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
