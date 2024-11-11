7f0f32900876a5596a63b7ec2584990571b9f0fb
ji-circle <sp202401jw@gmail.com>
2024-11-11 19:25:48 +0900

    finish: Loginpage ui 완성

--- app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
+++ app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
@@ -1,14 +1,22 @@
 package com.example.fifthapp.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fifthapp.ui.theme.FifthAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,10 +29,44 @@ fun LoginPage() {
             })
         }
     ) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                //아래것들 바깥으로 패딩 주기
+                .padding(16.dp)
+        ) {
+            //텍스트필드 2개와 버튼 하나 생성 - 로그인 정보 입력할부분
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(text = "E-mail")
+                }
+            )
+            //텍스트필드 간 공백 주기
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(text = "Password")
+                }
+            )
+            //텍스트필드-버튼 간 공백 주기
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                //버튼 위치를 오른쪽으로 보냈다
+                modifier = Modifier.align(Alignment.End),
+                onClick = {}
+            ) {
+                Text(text = "Login")
+            }
+        }
     }
 }
 
