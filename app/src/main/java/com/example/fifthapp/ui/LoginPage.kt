a4142666f0481290fe2143e318979afa70a5617f
ji-circle <sp202401jw@gmail.com>
2024-11-11 19:57:21 +0900

    finish: ui

--- app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
+++ app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -14,8 +15,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,6 +30,10 @@ import androidx.compose.ui.unit.dp
 fun LoginPage(
     goToRegisterPage: () -> Unit
 ) {
+    //상태 추가해주기 - 입력 위한
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
     Scaffold(modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(title = {
@@ -40,22 +51,26 @@ fun LoginPage(
             //텍스트필드 2개와 버튼 하나 생성 - 로그인 정보 입력할부분
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = {},
+                value = email,
+                onValueChange = { email = it },
                 label = {
                     Text(text = "E-mail")
-                }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
             )
             //텍스트필드 간 공백 주기
             Spacer(modifier = Modifier.height(8.dp))
 
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = {},
+                value = password,
+                onValueChange = { password = it },
                 label = {
                     Text(text = "Password")
-                }
+                },
+                //입력창에 입력된 비번이 안보이게 하고싶을때
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
             //텍스트필드-버튼 간 공백 주기
             Spacer(modifier = Modifier.height(8.dp))
