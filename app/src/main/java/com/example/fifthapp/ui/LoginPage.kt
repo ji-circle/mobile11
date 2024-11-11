c6e147ffe29535635a292db13607d81f41ca42b2
ji-circle <sp202401jw@gmail.com>
2024-11-11 19:18:58 +0900

    second

--- app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
+++ app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
@@ -2,17 +2,45 @@ package com.example.fifthapp.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.fifthapp.Greeting
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fifthapp.ui.theme.FifthAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginPage(){
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+fun LoginPage() {
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Login Page")
+            })
+        }
+    ) { innerPadding ->
         Greeting(
             name = "Android",
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FifthAppTheme {
+        Greeting("Android")
     }
 }
