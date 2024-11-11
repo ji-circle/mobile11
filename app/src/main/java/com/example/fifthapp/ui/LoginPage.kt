5f8b618c3480893b1dbca4a3627b97df10716431
ji-circle <sp202401jw@gmail.com>
2024-11-11 21:30:13 +0900

    finish: logout

--- app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
+++ app/src/main/java/com/example/fifthapp/ui/LoginPage.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -35,13 +39,34 @@ fun LoginPage(
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
 
-    val currentUser = FirebaseAuth.getInstance().currentUser
+    //여기 추가
+    var isLoggedIn by rememberSaveable { mutableStateOf(false) }
+
+    //여기 있던 currentUser를 이동함... recomposition 문제 때문에
+//    val currentUser = FirebaseAuth.getInstance().currentUser
 
     Scaffold(modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopAppBar(title = {
-                Text(text = "Login Page")
-            })
+            TopAppBar(
+                title = {
+                    Text(text = "Login Page")
+                },
+                actions = {
+                    IconButton(
+                        //여기 내용 추가
+                        onClick = {
+                            logout()
+                            isLoggedIn = false
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "logout"
+                        )
+
+                    }
+                }
+            )
         }
     ) { innerPadding ->
         Column(
@@ -83,23 +108,51 @@ fun LoginPage(
 
             Button(
                 modifier = Modifier.align(Alignment.End),
-                onClick = { loginUser(email, password) }
+                onClick = {
+                    //여기 내용 추가
+                    loginUser(email, password)
+                    isLoggedIn = true
+                }
             ) {
                 Text(text = "Login")
             }
 
-            (if (currentUser != null) currentUser.email else "No user")?.let {
-                Text(
-                    text = it,
-                    style = typography.titleLarge
-                )
+            //isLoggedIn의 값이 바뀌었을 때 이게 다시 recompositio이 일어나야 함
+            // 그 때 currentUser를 받아와서 그거의 email을 텍스트로 받아서 나타낸다
+            if (isLoggedIn){
+                val currentUser = FirebaseAuth.getInstance().currentUser
+
+                if (currentUser != null){
+                    currentUser.email?.let {
+                        Text(
+                            text = it,
+                            style = typography.titleLarge
+                        )
+                    }
+                }
             }
+            //기존 아래 코드를 위로 바꿈
+//            //isLoggedIn이 바뀌었고, currentUser이 null이 아닐 때 로 수정.
+//            (if (isLoggedIn && currentUser != null) currentUser.email else "No user")?.let {
+//                Text(
+//                    text = it,
+//                    style = typography.titleLarge
+//                )
+//            }
         }
     }
 }
+
 
 fun loginUser(email: String, password: String) {
     val auth = FirebaseAuth.getInstance()
 
     auth.signInWithEmailAndPassword(email, password)
+}
+
+//추가
+fun logout() {
+    val auth = FirebaseAuth.getInstance()
+
+    auth.signOut()
 }
